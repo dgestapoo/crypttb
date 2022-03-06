cb70740ca8e381d8d0eea11b466eceaa9b6a4698
dgestapoo <dgestapoo@gmail.com>
2022-03-06 18:21:53 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,26 @@
+<?php
+
+/* data mining */
+
+$sign = "UAkTO5ZjlEsYvpG8Yf9x1lYl0hIxuBd87urGaz0Qrgnk+NqqNXwuCWSRudtlOnZu2Yf1OrxFtrCBIRCF";
+$auth = "Bearer gAAAAABgrzfxGtpeZX3Ri_9_7ERKMkjHAohGEBnLQ447Gddvow7TFvuqW3HlnbjU3lNImuoHUa1K6B_1HQ5nfeN1ZPtjj9FzArnQp2fhYa0OCiJASmN6WGg=";
+
+/* data email */
+
+$sign1 = "fzwkHAU3J8zEaNeiFkvsyGSrPnmYvhp8d37RdvVdQPnNge+chHMPIXA6bLOpITOSyr/i3wsvG+UZaJ2k";
+$auth1 = "Bearer gAAAAABgrzfxGtpeZX3Ri_9_7ERKMkjHAohGEBnLQ447Gddvow7TFvuqW3HlnbjU3lNImuoHUa1K6B_1HQ5nfeN1ZPtjj9FzArnQp2fhYa0OCiJASmN6WGg=";
+
+
+/* cookie */
+
+$user_agent = "Mozilla/5.0 (Linux; Android 10; RMX2002) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.114 Mobile Safari/537.36";
+$ct_ts = "1622095857";
+$token = "gkkSzZpR6LG8vuttvOhMlP6Tno1fAcA7g30BErvfFBDvVyuXIRZmC4lWvKhd18cZ";
+$session = "a2aymzukjnopi1bs2d9ecqshf5p6f9gj";
+$bid = "bid=00bfc150-ba4f-11eb-9e5f-02420a03040e";
+$ct_v = "v=4.1.71";
+$ct_av = "av=2.0.0.144";
+
+
+
+?>
